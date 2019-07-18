c86535845a8a6d67ae68c362a18171e341fe1fed
srinivas0256 <48003682+srinivas0256@users.noreply.github.com>
2019-07-18 11:36:49 +0530

    both scale-up and scale-down autos-scaling group

--- 02-notifon/scale-up.py
+++ 02-notifon/scale-up.py
@@ -3,4 +3,11 @@ import boto3
 session = boto3.Session(profile_name='pythonAutomation')
 as_client = session.client('autoscaling')
 
-as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
+as_client.execute_policy(AutoScalingGroupName='Notifon Example', PolicyName='Scale Up')
+
+# to scale down auto-scaling group
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+as_client = session.client('autoscaling')
+
+as_client.execute_policy(AutoScalingGroupName='Notifon Example', PolicyName='Scale Down')
